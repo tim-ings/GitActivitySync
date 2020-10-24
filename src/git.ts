33b2e487ec240b1f43840f3180d5ebfc3dd517f4
Tim Ings <tim@tim-ings.com>
2020-10-25 01:02:49 +0800

    Adds author envs

--- src/git.ts
+++ src/git.ts
@@ -6,7 +6,7 @@ import { ContributionsMap } from './github';
 
 const gitService = (baseDir: string, remote: string, branch: string) => {
   try { fs.mkdirSync(baseDir, { recursive: true }); } catch { }
-  const git = simpleGit({ baseDir });
+  const git = simpleGit({ baseDir,  });
   process.on('exit', () => rimraf.sync(baseDir));
   const commitFile = `${baseDir}/README.md`;
 
@@ -19,16 +19,17 @@ const gitService = (baseDir: string, remote: string, branch: string) => {
     push: async () => {
       await git.push(remote, branch);
     },
-    commit: async (date: Date) => {
+    commit: async (date: Date, name: string, email: string) => {
       const message = `GitActivitySync: ${date.toDateString()}`;
+      const author = `${name} <${email}>`;
       fs.writeFileSync(commitFile, `# ${message}`);
-      await git.commit(message, '.', { '--date': date.toISOString() });
+      await git.commit(message, '.', { '--date': date.toISOString(), '--author': author });
     },
     log: async () =>  git.log(),
   }
 }
 
-export const syncContributions = async (remote: string, contributions: ContributionsMap, branch: string) => {
+export const syncContributions = async (remote: string, contributions: ContributionsMap, branch: string, name: string, email: string) => {
   const tmpDir = `./tmp/${uuid()}`;
   const { pull, push, commit, log } = gitService(tmpDir, remote, branch);
 
@@ -42,7 +43,7 @@ export const syncContributions = async (remote: string, contributions: Contribut
   for (const [date, count] of contributions.entries()) {
     if (existingDates.has(date.toDateString())) continue;
     for (let i = 0; i < count; i++) {
-      await commit(date);
+      await commit(date, name, email);
       console.log(`Committed ${date}`);
     }
   }
--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,18 @@ import { loadContributions } from './github';
 
 const throwError = (msg?: string) => { throw Error(msg) };
 const env = {
-  sourceGithubUser: process.argv[2] ?? process.env.SOURCE_USER ?? throwError('source github user not provided'),
-  destinationGitRemote: process.argv[3] ?? process.env.DESTINATION_REMOTE ?? throwError('destination remote not provided'),
-  branch: process.argv[4] ?? process.env.DESTINATION_BRANCH ?? throwError('destination branch not provided'),
+  sourceGithubUser: process.env.SOURCE_USER ?? throwError('source github user not provided'),
+  destinationGitRemote: process.env.DESTINATION_REMOTE ?? throwError('destination remote not provided'),
+  authorName: process.env.GIT_AUTHOR_NAME ?? throwError('git author name not provided'),
+  authorEmail: process.env.GIT_AUTHOR_EMAIL ?? throwError('git author email not provided'),
+  branch: process.env.DESTINATION_BRANCH ?? throwError('destination branch not provided'),
+  deployKey: process.env.DEPLOY_KEY ?? throwError('deploy key not provided'),
 }
 
 const main = async () => {
   const sourceContributions = await loadContributions(env.sourceGithubUser);
   console.log(`Found ${sourceContributions.size} contributions for ${env.sourceGithubUser}`);
-  await syncContributions(env.destinationGitRemote, sourceContributions, env.branch);
+  await syncContributions(env.destinationGitRemote, sourceContributions, env.branch, env.authorName, env.authorEmail);
 }
 
 main();
